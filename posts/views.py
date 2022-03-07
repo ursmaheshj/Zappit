74192d816279b57b707fcf84ca27a58d1520e17d
Mahesh Jadhav <omkar28399@gmail.com>
2022-03-07 20:59:25 +0530

    api working successfully

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from posts.serializers import postSerializer
+from .models import Post
+
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = postSerializer
+    
