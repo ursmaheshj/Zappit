2933da3518d270c50e888ef964a112c364b15d38
Mahesh Jadhav <omkar28399@gmail.com>
2022-03-08 00:04:03 +0530

    django rest api validation(Login) added

--- zappit/urls.py
+++ zappit/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts',views.PostList.as_view() ),
-    path('api/posts/<int:pk>/vote',views.VoteList.as_view())
+    path('api/posts/<int:pk>/vote',views.VoteList.as_view()),
+    path('api-auth',include('rest_framework.urls'))
 ]
