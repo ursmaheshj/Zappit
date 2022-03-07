5c02d445b91367bbf4812b2545b565bfedeefd2d
Mahesh Jadhav <omkar28399@gmail.com>
2022-03-08 00:24:42 +0530

    updated api so it can delete the votes

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.exceptions import ValidationError
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from rest_framework.response import Response
+from rest_framework import generics,permissions,mixins,status
 from posts.serializers import postSerializer, voteSerializer
 from .models import Post, Vote
 
@@ -26,3 +27,11 @@ class VoteList(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted for this post :)')
         serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self,request,*args,**kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted on this...Silly!')
+
